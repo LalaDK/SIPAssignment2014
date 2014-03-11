06e9b1ef58d9c2f10bcdf3b93a9665ae7d16dc96
Mads <Mads@Mads-Laptop>
2014-03-11 12:44:30 +0100

    Updated

--- src/java/utilities/Utilities.java
+++ src/java/utilities/Utilities.java
@@ -18,27 +18,11 @@ import java.util.HashMap;
 public class Utilities {
 
     public static HashMap<String, Integer> getSatisfaction(ArrayList<Subject> subjects, ArrayList<Person> persons) {
-
-        HashMap<String, Integer> result = new HashMap<>();
-        result.put("very_satisfied", 0);
-        result.put("satisfied", 0);
-        result.put("above_average", 0);
-        result.put("below_average", 0);
-        result.put("unsatisfied", 0);
-        result.put("very_unsatisfied", 0);
-
+        HashMap<String, Integer> result = getLevelsOfSatisfaction();
         ArrayList<Subject> poolA = new ArrayList<>();
         ArrayList<Subject> poolB = new ArrayList<>();
 
-        for (Subject s : subjects) {
-            if (s.getPool() == 'a') {
-                poolA.add(s);
-            }
-
-            if (s.getPool() == 'b') {
-                poolB.add(s);
-            }
-        }
+        sortSubjectsIntoPools(subjects, poolA, poolB);
 
         for (Person p : persons) {
 
@@ -46,13 +30,7 @@ public class Utilities {
             ArrayList<Vote> firstPriority = new ArrayList<>();
             ArrayList<Vote> secondPriority = new ArrayList<>();
 
-            for (Vote v : p.getVoteCollection()) {
-                if (v.getPriority().intValue() == 1) {
-                    firstPriority.add(v);
-                } else {
-                    secondPriority.add(v);
-                }
-            }
+            prioritizeVotes(p, firstPriority, secondPriority);
 
             // Fullfillments
             boolean firstPriorityFullfilledPoolA = false;
@@ -63,7 +41,8 @@ public class Utilities {
             // First Priority Pool A
             for (Vote vote : firstPriority) {
                 for (Subject subject : poolA) {
-                    if (!firstPriorityFullfilledPoolA && vote.getSubject().getSId() == subject.getSId()) {
+                    if (!firstPriorityFullfilledPoolA
+                            && vote.getSubject().getSId() == subject.getSId()) {
                         firstPriorityFullfilledPoolA = true;
                     }
                 }
@@ -72,7 +51,8 @@ public class Utilities {
             // First Priority Pool B
             for (Vote vote : firstPriority) {
                 for (Subject subject : poolB) {
-                    if (!firstPriorityFullfilledPoolB && vote.getSubject().getSId() == subject.getSId()) {
+                    if (!firstPriorityFullfilledPoolB
+                            && vote.getSubject().getSId() == subject.getSId()) {
                         firstPriorityFullfilledPoolB = true;
                     }
                 }
@@ -81,7 +61,8 @@ public class Utilities {
             // Second Priority Pool A
             for (Vote vote : secondPriority) {
                 for (Subject subject : poolA) {
-                    if (!firstPriorityFullfilledPoolA && vote.getSubject().getSId() == subject.getSId()) {
+                    if (!firstPriorityFullfilledPoolA
+                            && vote.getSubject().getSId() == subject.getSId()) {
                         secondPriorityFullfilledPoolA = true;
                     }
                 }
@@ -90,39 +71,75 @@ public class Utilities {
             // Second Priority Pool B
             for (Vote vote : secondPriority) {
                 for (Subject subject : poolB) {
-                    if (!firstPriorityFullfilledPoolB && vote.getSubject().getSId() == subject.getSId()) {
+                    if (!firstPriorityFullfilledPoolB
+                            && vote.getSubject().getSId() == subject.getSId()) {
                         secondPriorityFullfilledPoolB = true;
                     }
                 }
             }
+            decideLevelOfSatisfaction(
+                    firstPriorityFullfilledPoolA,
+                    firstPriorityFullfilledPoolB, result,
+                    secondPriorityFullfilledPoolB,
+                    secondPriorityFullfilledPoolA);
+        }
 
-            /*
-             boolean firstPriorityFullfilledPoolA = false;
-             boolean firstPriorityFullfilledPoolB = false;
-             boolean secondPriorityFullfilledPoolA = false;
-             boolean secondPriorityFullfilledPoolB = false;
-             */
-            if (firstPriorityFullfilledPoolA && firstPriorityFullfilledPoolB) {
-                result.put("very_satisfied", result.get("very_satisfied") + 1);
-            } else if ((firstPriorityFullfilledPoolA && secondPriorityFullfilledPoolB)
-                    || (firstPriorityFullfilledPoolB && secondPriorityFullfilledPoolA)) {
-                result.put("satisfied", result.get("satisfied") + 1);
-            } else if (secondPriorityFullfilledPoolA && secondPriorityFullfilledPoolB) {
-                result.put("above_average", result.get("above_average") + 1);
-            } else if (firstPriorityFullfilledPoolA || firstPriorityFullfilledPoolB) {
-                result.put("below_average", result.get("below_average") + 1);
-            } else if (secondPriorityFullfilledPoolA || secondPriorityFullfilledPoolB) {
-                result.put("unsatisfied", result.get("unsatisfied") + 1);
+        return result;
+    }
+
+    private static void prioritizeVotes(Person p, ArrayList<Vote> firstPriority, 
+            ArrayList<Vote> secondPriority) {
+        for (Vote v : p.getVoteCollection()) {
+            if (v.getPriority().intValue() == 1) {
+                firstPriority.add(v);
             } else {
-                result.put("very_unsatisfied", result.get("very_unsatisfied") + 1);
+                secondPriority.add(v);
             }
         }
+    }
 
-        // Print result for testing
-        for (String k : result.keySet()) {
-            System.out.println(result.get(k) + " - " + k);
+    private static void decideLevelOfSatisfaction(
+            boolean firstPriorityFullfilledPoolA, 
+            boolean firstPriorityFullfilledPoolB, 
+            HashMap<String, Integer> result, 
+            boolean secondPriorityFullfilledPoolB, 
+            boolean secondPriorityFullfilledPoolA) {
+        if (firstPriorityFullfilledPoolA && firstPriorityFullfilledPoolB) {
+            result.put("very_satisfied", result.get("very_satisfied") + 1);
+        } else if ((firstPriorityFullfilledPoolA && secondPriorityFullfilledPoolB)
+                || (firstPriorityFullfilledPoolB && secondPriorityFullfilledPoolA)) {
+            result.put("satisfied", result.get("satisfied") + 1);
+        } else if (secondPriorityFullfilledPoolA && secondPriorityFullfilledPoolB) {
+            result.put("above_average", result.get("above_average") + 1);
+        } else if (firstPriorityFullfilledPoolA || firstPriorityFullfilledPoolB) {
+            result.put("below_average", result.get("below_average") + 1);
+        } else if (secondPriorityFullfilledPoolA || secondPriorityFullfilledPoolB) {
+            result.put("unsatisfied", result.get("unsatisfied") + 1);
+        } else {
+            result.put("very_unsatisfied", result.get("very_unsatisfied") + 1);
         }
+    }
 
+    private static void sortSubjectsIntoPools(ArrayList<Subject> subjects, ArrayList<Subject> poolA, ArrayList<Subject> poolB) {
+        for (Subject s : subjects) {
+            if (s.getPool() == 'a') {
+                poolA.add(s);
+            }
+
+            if (s.getPool() == 'b') {
+                poolB.add(s);
+            }
+        }
+    }
+
+    private static HashMap<String, Integer> getLevelsOfSatisfaction() {
+        HashMap<String, Integer> result = new HashMap<>();
+        result.put("very_satisfied", 0);
+        result.put("satisfied", 0);
+        result.put("above_average", 0);
+        result.put("below_average", 0);
+        result.put("unsatisfied", 0);
+        result.put("very_unsatisfied", 0);
         return result;
     }
 }
