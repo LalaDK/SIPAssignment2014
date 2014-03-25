28d134d48b630e7d4cad531e81f08a262b5f95de
Thomas <Thomas@LSC-Thomas>
2014-03-25 22:05:46 +0100

    DTO in the making

--- src/java/dataTransferObjects/DtoSubject.java
+++ src/java/dataTransferObjects/DtoSubject.java
@@ -11,7 +11,72 @@ package dataTransferObjects;
  * @author Thomas
  */
 public class DtoSubject {
-    private int id;
-    private String name;
+    private int sId;
+    private String subjectname;
     private String description;
+    private int firstPriorityVotes;
+    private int secondPriorityVotes;
+    private String pool;
+
+    public DtoSubject() {
+    }
+
+    public DtoSubject(int sId, String subjectname, String description, int firstPriorityVotes, int secondPriorityVotes, String pool) {
+        this.sId = sId;
+        this.subjectname = subjectname;
+        this.description = description;
+        this.firstPriorityVotes = firstPriorityVotes;
+        this.secondPriorityVotes = secondPriorityVotes;
+        this.pool = pool;
+    }
+
+    public int getSId() {
+        return sId;
+    }
+
+    public void setSId(int sId) {
+        this.sId = sId;
+    }
+
+    public String getSubjectname() {
+        return subjectname;
+    }
+
+    public void setSubjectname(String subjectname) {
+        this.subjectname = subjectname;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getFirstPriorityVotes() {
+        return firstPriorityVotes;
+    }
+
+    public void setFirstPriorityVotes(int firstPriorityVotes) {
+        this.firstPriorityVotes = firstPriorityVotes;
+    }
+
+    public int getSecondPriorityVotes() {
+        return secondPriorityVotes;
+    }
+
+    public void setSecondPriorityVotes(int secondPriorityVotes) {
+        this.secondPriorityVotes = secondPriorityVotes;
+    }
+
+    public String getPool() {
+        return pool;
+    }
+
+    public void setPool(String pool) {
+        this.pool = pool;
+    }
+
+    
 }
--- src/java/utilities/DtoAssembler.java
+++ src/java/utilities/DtoAssembler.java
@@ -3,13 +3,44 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package utilities;
+
+import dataTransferObjects.DtoSubject;
+import entities.Subject;
+import entities.Vote;
+import java.util.ArrayList;
 
 /**
  *
  * @author Thomas
  */
 public class DtoAssembler {
+
+    private static DtoSubject subjectToDtoSubject(Subject subject) {
+        int firstPriority = 0;
+        int secondPriority = 0;
+        DtoSubject dto = new DtoSubject();
+        dto.setSId(subject.getSId().intValue());
+        dto.setSubjectname(subject.getSubjectname());
+        dto.setDescription(subject.getDescription());
+        dto.setPool(subject.getPool().toString());
+        for (Vote v : subject.getVoteCollection()) {
+            if (v.getPriority().intValue() == 1) {
+                firstPriority++;
+            } else if (v.getPriority().intValue() == 2) {
+                secondPriority++;
+            }
+        }
+        dto.setFirstPriorityVotes(firstPriority);
+        dto.setSecondPriorityVotes(secondPriority);
+        return dto;
+    }
     
+    private static ArrayList<DtoSubject> subjectsToDtoSubjects(ArrayList<Subject> subjects){
+        ArrayList<DtoSubject> dtoSubjects = new ArrayList();
+        for(Subject s: subjects){
+            dtoSubjects.add(subjectToDtoSubject(s));
+        }
+        return dtoSubjects;
+    }
 }
